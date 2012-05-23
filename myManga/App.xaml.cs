7df107d177a2f833c40ab9dceda33d6dade62af3
ProbCaboose_cp <jarijop@gmail.com>
2012-05-23 19:20:30 +0000

    Deleted Unused File.

---
+++ myManga/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using myManga.ViewModels;
+using System.Diagnostics;
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using BakaBox.DLL;
+
+namespace myManga
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    [DebuggerStepThrough]
+    public partial class App : Application
+    {
+        public App()
+        {
+            EmbeddedDLLs.Instance.SetEmbeddedDLLResourceLocation("Resources.DLLs");
+            AppDomain.CurrentDomain.AssemblyResolve += EmbeddedDLLs.Instance.ResolveAssembly;
+            InitializeComponent();
+        }
+    }
+}

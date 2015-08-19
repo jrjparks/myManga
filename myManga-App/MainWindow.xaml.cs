2f5d7147f32613ed8d94666f26b63b93a57c110b
James R. Parks <jarijop@gmail.com>
2015-08-19 00:01:53 -0400

    Added fullscreen support

--- myManga-App/MainWindow.xaml.cs
+++ myManga-App/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using myManga_App.Properties;
-using myManga_App.Themes.Window;
+using myManga_App.Controls.Window;
 
 namespace myManga_App
 {

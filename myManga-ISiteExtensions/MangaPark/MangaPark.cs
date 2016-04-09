ed62db112c5cd79b0f5f703d7e14186d1b7415b6
James R. Parks <jarijop@gmail.com>
2016-04-08 20:16:50 -0400

    Adding MangaPark.me

---
+++ myManga-ISiteExtensions/MangaPark/MangaPark.cs
@@ -0,0 +1,111 @@
+﻿using HtmlAgilityPack;
+using myMangaSiteExtension.Attributes;
+using myMangaSiteExtension.Enums;
+using myMangaSiteExtension.Interfaces;
+using myMangaSiteExtension.Objects;
+using myMangaSiteExtension.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Runtime.Caching;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace MangaPark
+{
+    [IExtensionDescription(
+        Name = "MangaPark",
+        URLFormat = "mangapark.me",
+        RefererHeader = "https://www.mangapark.me/",
+        RootUrl = "https://www.mangapark.me",
+        Author = "James Parks",
+        Version = "0.0.1",
+        SupportedObjects = SupportedObjects.All,
+        Language = "English",
+        RequiresAuthentication = false)]
+    public class MangaPark : ISiteExtension
+    {
+        #region IExtesion
+        private IExtensionDescriptionAttribute EDA;
+        public IExtensionDescriptionAttribute ExtensionDescriptionAttribute
+        { get { return EDA ?? (EDA = GetType().GetCustomAttribute<IExtensionDescriptionAttribute>(false)); } }
+
+        private Icon extensionIcon;
+        public Icon ExtensionIcon
+        {
+            get
+            {
+                if (Equals(extensionIcon, null)) extensionIcon = Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location);
+                return extensionIcon;
+            }
+        }
+
+        public CookieCollection Cookies
+        { get; private set; }
+
+        public Boolean IsAuthenticated
+        { get; private set; }
+
+        public bool Authenticate(NetworkCredential credentials, CancellationToken ct, IProgress<Int32> ProgressReporter)
+        {
+            if (IsAuthenticated) return true;
+            throw new NotImplementedException();
+        }
+
+        public void Deauthenticate()
+        {
+            if (!IsAuthenticated) return;
+            Cookies = null;
+            IsAuthenticated = false;
+        }
+
+        public List<MangaObject> GetUserFavorites()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AddUserFavorites(MangaObject MangaObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveUserFavorites(MangaObject MangaObject)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region ISiteExtension
+        public MangaObject ParseMangaObject(string Content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ChapterObject ParseChapterObject(string Content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PageObject ParsePageObject(string Content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SearchRequestObject GetSearchRequestObject(string SearchTerm)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<SearchResultObject> ParseSearch(string Content)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
